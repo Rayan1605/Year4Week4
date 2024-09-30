c2f093318fd2a3ee71ef0bcb9c11220b271e1302
G00394595@atu.ie <rayanzyad99@gmail.com>
2024-09-30 14:05:57 +0100

    Start

--- src/main/java/com/example/year4week3/ErrorHandler/ExceptionHandler.java
+++ src/main/java/com/example/year4week3/ErrorHandler/ExceptionHandler.java
@@ -1,17 +1,29 @@
 package com.example.year4week3.ErrorHandler;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handlevalidation(MethodArgumentNotValidException e){
-        return "Error occurred. " + e.getBindingResult().getFieldError().getDefaultMessage();
-        }
+    public Map<String, String> handlevalidation(MethodArgumentNotValidException e){
+
+      Map<String, String> errors = new HashMap<>();
+      for (FieldError error: e.getBindingResult().getFieldErrors()){
+          String fieldName = error.getField() + " ";
+          String errorMessage = error.getDefaultMessage() + "  ";
+          errors.put(fieldName, errorMessage);
+      }
+      return errors;
+    }
 
 }
--- src/main/java/com/example/year4week3/Year4Week3Application.java
+++ src/main/java/com/example/year4week3/Year4Week3Application.java
@@ -2,8 +2,10 @@ package com.example.year4week3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients //This is to enable feign clients
 public class Year4Week3Application {
 
     public static void main(String[] args) {
